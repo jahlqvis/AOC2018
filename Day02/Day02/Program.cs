f0427cf5306696daf2e690d9a9280ade03b7b488
Johan <jahlqvis@gmail.com>
2019-01-03 22:45:14 +0200

    day 2 part 2 solved

--- Day02/Day02/Program.cs
+++ Day02/Day02/Program.cs
@@ -10,6 +10,8 @@ namespace Day02
         public List<string> rows;
         public int tuples = 0;
         public int triples = 0;
+        public string a="";
+        public string b="";
 
 
 
@@ -29,24 +31,27 @@ namespace Day02
         public bool CountChars()
         {
             register = new Dictionary<char, int>();
-
+            int pos;
             foreach(string row in rows)
             {
+                pos = 0; 
                 foreach(char c in row)
                 {
                     if (register.ContainsKey(c))
                     {
-                        int val;
-                        register.TryGetValue(c, out val);
+                        int x;
+                        register.TryGetValue(c, out x);
                         register.Remove(c);
-                        val++;
-                        register.Add(c, val);
+                        x++;
+                        register.Add(c, x);
 
                     }
                     else
                     {
+
                         register.Add(c, 1);
                     }
+                    pos++;
                 }
 
                 if (register.ContainsValue(2))
@@ -62,6 +67,43 @@ namespace Day02
 
         }
 
+        public bool FindPairs()
+        {
+            register = new Dictionary<char, int>();
+
+            foreach (string row in rows)
+            {
+                foreach(string row2 in rows)
+                {
+                    int d = 0;
+                    int mark = 0;
+                    for(int i=0;i<row.Length && i<row2.Length;i++)
+                    {
+                        if (row[i] != row2[i])
+                        {
+                            d++;
+                            mark = i;
+                        }
+
+                    }
+                   
+                    if(d == 1)
+                    {
+
+                        a = row.Substring(0, mark);
+                        b = row.Substring(mark + 1, row.Length - mark - 1);
+
+                        return true;    
+                    }
+                }
+
+            }
+
+
+
+            return true;
+
+        }
     }
 
     class MainClass
@@ -72,9 +114,21 @@ namespace Day02
         {
             ChecksumFinder cf = new ChecksumFinder();
             cf.Import("input.txt");
-            cf.CountChars();
+            //cf.CountChars();
+            cf.FindPairs();
 
-            Console.WriteLine("Hello World! Checksum is {0}", cf.tuples * cf.triples);
+            Console.WriteLine("Lines: {0}", cf.rows.Count);
+
+            //Console.WriteLine("Hello World! Checksum is {0}", cf.tuples * cf.triples);
+            //foreach (var item in cf.register)
+            //{
+
+            //    if (item.Value >= cf.rows.Count ) 
+            //        Console.WriteLine("{0} {1}", item.Key, item.Value);
+            //}
+
+            Console.WriteLine("a: {0} b: {1}", cf.a, cf.b);
+
         }
     }
 }
